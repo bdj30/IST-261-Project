c9fa4d82e042b954ecc5a7fc96e6f59bed0fa430
bdj30 <72350174+bdj30@users.noreply.github.com>
2023-02-07 14:56:33 -0500

    Update BMICalculator.java

--- BMICalculator.java
+++ BMICalculator.java
@@ -5,11 +5,9 @@ public class BMICalculator {
       Scanner sc = new Scanner(System.in);
       System.out.print("Input weight in kilogram: ");
       double weight = sc.nextDouble();
-      System.out.print("
-Input height in meters: ");
+      System.out.print("Input height in meters: ");
       double height = sc.nextDouble();
       double BMI = weight / (height * height);
-      System.out.print("
-The Body Mass Index (BMI) is " + BMI + " kg/m2");
+      System.out.print("The Body Mass Index (BMI) is " + BMI + " kg/m2");
    }
 }
