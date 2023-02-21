63cd3d3d4deabee43300ad144548898c00197600
bdj30 <72350174+bdj30@users.noreply.github.com>
2023-02-20 21:41:24 -0500

    Update TestHarness.java

--- TestHarness.java
+++ TestHarness.java
@@ -30,5 +30,7 @@ public class TestHarness {
     }
     
     public testClassHierarchy() {
-        
+        ArrayList<Object> myArrayList = new ArrayList<Object>();
+        myArrayList.add(
+        myArrayList.add(
 }
