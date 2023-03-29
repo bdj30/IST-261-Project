91d3d342b24186ad105710991a946cd70b2bc4ca
bdj30 <72350174+bdj30@users.noreply.github.com>
2023-03-28 21:11:45 -0400

    Update GuiController.java

--- GuiController.java
+++ GuiController.java
@@ -11,6 +11,18 @@ public class GuiController {
         model.form().getDateText().setText(data.getDate().toString());
         //updates the Bmi text field
         model.form().getBmiText().setText(data.getStringBMI());
+        
+        model.form().getPreviousButton().addActionListener(e -> {
+            TrackData lastEntry = data.getLastEntry();
+            if (lastEntry != null) {
+                // update the data and GUI form with the last entry
+                data = lastEntry;
+                model.form().getDateText().setText(data.getDate().toString());
+                model.form().getBmiText().setText(data.getStringBMI());
+            } else {
+                JOptionPane.showMessageDialog(model.form(), "No previous entry available");
+            }
+        });
     }
 
 }
